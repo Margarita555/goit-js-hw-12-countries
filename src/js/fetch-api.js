49fbe727aadaa0e458da6e79577414d3533ad9fb
Margarita555 <a_rita@ukr.net>
2021-09-04 12:54:13 +0300

    +

--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,6 +1,7 @@
 import debounce from 'lodash.debounce';
 // import '../css/common.css';
-import fetchCountries from './fetchCountries';
+// import fetchCountries from './fetchCountries';
+import countryTemplate from '../templates/country-template.hbs';
 
 const inputEl = document.querySelector('.input');
 // const countriesList = document.querySelector('.countries-list');
@@ -10,4 +11,39 @@ function onInputChange() {
     let inputValue = inputEl.value;
     console.log(inputValue)
     fetchCountries(inputValue)
+}
+
+
+function fetchCountries(countryName) {
+    fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+        .then(response => {
+        return response.json();
+    }).then(renderCountry)
+        .catch(error => {
+        console.log(error);
+        })
+        // .finally(() => countryItem.reset)
+    
+}
+const countriesList = document.querySelector('.countries-list');
+const countryItem = document.querySelector('.country-item');
+
+function renderCountry(countries) {
+    countryItem.innerHTML = '';
+    if (countries.length > 10) {
+        console.log('pnotify');
+    } else if (countries.length > 2 && countries.length < 10) {
+        const makeCountriesList = countries.map(country => {
+            const liEl = document.createElement('li');
+            liEl.textContent = country.name;
+            return liEl
+        })
+        countriesList.append(...makeCountriesList)
+        
+    } else if (countries.length === 1) {
+        countriesList.innerHTML = '';
+        console.log(countries);
+        const markup = countryTemplate(countries)
+        countryItem.insertAdjacentHTML('beforeend', markup)
+    }
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,44 +1,34 @@
-export default fetchCountries ;
+// export default fetchCountries ;
 
-function fetchCountries(countryName) {
-    // console.log('r')
-    fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
-        .then(response => {
-        return response.json();
-    }).then(renderCountry)
-        .catch(error => {
-        console.log(error);
-    })
+// function fetchCountries(countryName) {
+//     // console.log('r')
+//     fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+//         .then(response => {
+//         return response.json();
+//     }).then(renderCountry)
+//         .catch(error => {
+//         console.log(error);
+//     })
     
-}
-const countriesList = document.querySelector('.countries-list')
+// }
+// const countriesList = document.querySelector('.countries-list')
 
-function renderCountry(countries) {
-    if (countries.length > 10) {
-        console.log('pnotify');
-    } else if (countries.length > 2 && countries.length < 10) {
-        console.log('2,10');
-        const makeCountriesList = countries.map(country => {
-            const liEl = document.createElement('li');
-            liEl.textContent = country.name;
-            return liEl
-        })
-        countriesList.append(...makeCountriesList)
+// function renderCountry(countries) {
+//     if (countries.length > 10) {
+//         console.log('pnotify');
+//     } else if (countries.length > 2 && countries.length < 10) {
+//         console.log('2,10');
+//         const makeCountriesList = countries.map(country => {
+//             const liEl = document.createElement('li');
+//             liEl.textContent = country.name;
+//             return liEl
+//         })
+//         countriesList.append(...makeCountriesList)
         
-    } else if (countries.length === 1) {
-        console.log(countries);
-    }
-}
+//     } else if (countries.length === 1) {
+//         console.log(countries);
+//     }
+// }
 
 
-//  fetchCountries()
-// import API from '.index.js'
 
-// API.fetch('https://restcountries.eu/rest/v2/name/{name}').then(response => {
-//     console.log(response.json());
-//     return response.json();
-// }).then(name => {
-//     console.log(name);
-// }).catch(error => {
-//     console.log(error);
-//    })
